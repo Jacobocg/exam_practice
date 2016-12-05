48ba360629c9d32a8ffada71c6c9dec05c408e58
Jacobo Cortina <jacobcg@gmail.com>
2016-12-05 23:41:24 +0100

    add question counter to navbar component

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,11 +1,15 @@
 // npm packages
 import React from 'react';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
 
 import {Logout} from '../logout';
 
+const mapStateToProps = (state) => ({
+  numQuestions: state.questions.questions.length,
+});
 
-export default () => (
+const Navbar = ({numQuestions}) => (
   <nav className="navbar navbar-default">
     <div className="container-fluid">
       <div className="navbar-header">
@@ -14,7 +18,7 @@ export default () => (
 
       <ul className="nav navbar-nav">
         <li>
-          <Link to="/">Browse questions</Link>
+          <Link to="/">Browse questions <span className="badge">{numQuestions}</span></Link>
         </li>
         <li>
           <Link to="/create">Create new question</Link>
@@ -26,3 +30,5 @@ export default () => (
     </div>
   </nav>
 );
+
+export default connect(mapStateToProps, null)(Navbar);
