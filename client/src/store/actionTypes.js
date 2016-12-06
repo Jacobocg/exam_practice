9ccb54bdab941baf4cb44c3b19ef82dfc043bfd3
Jacobo Cortina <jacobcg@gmail.com>
2016-12-06 14:23:44 +0100

    added logout notification with an Epic

--- client/src/store/actionTypes.js
+++ client/src/store/actionTypes.js
@@ -4,6 +4,7 @@ export const HELLO_WORLD_END = 'HELLO_WORLD_END';
 // auth actions
 export const DO_LOGIN = 'DO_LOGIN';
 export const DO_LOGOUT = 'DO_LOGOUT';
+export const DO_LOGOUT_SUCCESS = 'DO_LOGOUT_SUCCESS';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const DO_REGISTER = 'DO_REGISTER';
